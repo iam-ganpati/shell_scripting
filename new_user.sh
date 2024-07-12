1a5aa1089e27513d49fcde48bfec33b70c67eff7
Ganpati Kolhal <154478305+iam-ganpati@users.noreply.github.com>
2024-07-13 00:02:19 +0530

    Update new_user.sh

--- new_user.sh
+++ new_user.sh
@@ -1,5 +1,10 @@
 # id "$username" &>/dev/null runs the id command and redirects any output or error messages to /dev/null, effectively making the command silent.
 
+# The &>/dev/null construct in shell scripting is used to redirect both the standard output (stdout) and standard error (stderr) of a command to /dev/null, 
+# effectively discarding any output or error messages produced by the command. This is useful when you want to suppress all output from a command.
+
+# /dev/null is a special file on Unix-like systems that discards all data written to it.
+
 #!/bin/bash 
 username="newuser" 
  
